e3a6bf9930786f4db40ca38130a3c15df7cbf24e
GK <donotspam@pls.ok>
2025-03-14 01:19:17 +0300

    Sh! It happens

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"judge/game"
 	"judge/player"
+	"os"
 )
 
 const (
@@ -28,18 +29,19 @@ func main() {
 	player1, err := player.NewPlayer(flag.Arg(1))
 	if err != nil {
 		fmt.Println(fmt.Errorf("error creating player 1: %w", err))
-		return
+		os.Exit(1)
 	}
 
 	player2, err := player.NewPlayer(flag.Arg(2))
 	if err != nil {
 		fmt.Println(fmt.Errorf("error creating player 2: %w", err))
-		return
+		os.Exit(1)
 	}
 
 	err = game.Play(flag.Arg(0), int(*count), player1, player2, *verbose)
 	if err != nil {
 		fmt.Println("error playing round:", err)
+		os.Exit(1)
 	}
 
 	if *verbose {
